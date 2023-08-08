125456b30685db844bf83cb84edc5a00ae26188f
Vladimir Burkov <vladimir.burkov@telekom.com>
2023-08-08 10:40:03 +0300

    init project

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from'./App.module.scss';
+
+console.log(styles);
+
+function App() {
+  return (
+    <div className={styles.app}>
+      heelloo
+    </div>
+  );
+}
+
+export default App;
